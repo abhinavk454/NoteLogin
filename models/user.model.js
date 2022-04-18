9169dc38a24dedadbdc5aea7139eeadfb53e07f7
Abhinav Kumar <abhinav.vikash2@gmail.com>
2022-04-18 11:47:27 +0530

    Model creation done

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+
+const Schema = require("mongoose").Schema;
+
+const User = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", User);
